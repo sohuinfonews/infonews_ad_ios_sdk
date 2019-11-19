3b5fa89a09833a94e5afbfc6bcd3e69c8032e458
DoubleShawn <462594734@qq.com>
2019-11-19 19:29:54 +0800

    0.0.3的版本

--- SINAdFramework/SINSSPADFramework.framework/Headers/SINAdManager.h
+++ SINAdFramework/SINSSPADFramework.framework/Headers/SINAdManager.h
@@ -4,7 +4,7 @@
 //
 //  Created by JiaLei on 2019/10/17.
 //  Copyright © 2019 Sohu. All rights reserved.
-//  2019年11月19日15:23:36
+//  2019年11月19日19:27:54
 
 #import <Foundation/Foundation.h>
 #import "SINAdManager.h"
--- SINAdFramework/SINSSPADFramework.framework/Headers/SINAdRelatedView.h
+++ SINAdFramework/SINSSPADFramework.framework/Headers/SINAdRelatedView.h
@@ -12,13 +12,14 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+// 目前有内存泄露的bug。
 @interface SINAdRelatedView : UIView
 
 /// 广告展示的label。可以在外部修改属性
-@property (nonatomic, strong, readonly, nullable) UILabel *adLabel;
+@property (nonatomic, weak, readonly, nullable) UILabel *adLabel;
  
 /// 广告展示的imageView。可以在外部修改属性
-@property (nonatomic, strong, readonly, nullable) UIImageView *logoImageView;
+@property (nonatomic, weak, readonly, nullable) UIImageView *logoImageView;
 
 // 必传（用于广告落地页跳转）
 @property (nonatomic, weak) UIViewController *rootViewController;
